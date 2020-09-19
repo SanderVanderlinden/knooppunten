fdc1abcf3658b41e0d2f825e9ea7151cdf0e1388
SanderVanderlinden <sanderVanderlinden@8e86bfb0e784>
2020-09-19 19:58:59 +0000

    Add bootstrap - work in progress

--- static/js/copy_to_clipboard.js
+++ static/js/copy_to_clipboard.js
@@ -1,11 +1,10 @@
 function myCopyFunction() {
     var copyText = document.getElementById("copy-to-clipboard");
+    copyText.style.visibility = "visible";
     copyText.select();
     copyText.setSelectionRange(0, 99999)
     document.execCommand("copy");
-
-    var tooltip = document.getElementById("myTooltip");
-    tooltip.innerHTML = "Gekopieerd naar het klembord!";
+    copyText.style.visibility = "hidden";
 }
 
 function dontClickFunction() {
