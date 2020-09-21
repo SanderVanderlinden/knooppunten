c26a7685ce2af833229b512e52aad07adfc2b655
SanderVanderlinden <sanderVanderlinden@8e86bfb0e784>
2020-09-21 13:17:48 +0000

    Add bootstrap - work in progress

--- static/js/knooppunt_post.js
+++ static/js/knooppunt_post.js
@@ -40,42 +40,47 @@ function showRoute() {
     link_naar_van_kies_bestemming.style.display = "none";
     link_naar_van_mijn_locatie_kies_bestemming.style.display = "none";
 
+    $("#copyRoute").show();
+
     if (!start.checked  && !end.checked && !reverse.checked){
         link_van_naar.style.display = "block";
-        $(copy-route).text = $(link_van_naar).attr('href');
+        $("#copyRoute").val($(link_van_naar).attr('href'));
     }
     else if (start.checked  && !end.checked && !reverse.checked){
         link_van_naar_mijn_locatie.style.display = "block";
-        $(copy-route).text = $(link_van_naar).attr('href');
+        $("#copyRoute").val($(link_van_naar_mijn_locatie).attr('href'));
     }
     else if (!start.checked  && end.checked && !reverse.checked){
         link_van_naar_kies_bestemming.style.display = "block";
-        $(copy-route).text = $(link_van_naar).attr('href');
+        $("#copyRoute").val($(link_van_naar_kies_bestemming).attr('href'));
     }
     else if (start.checked  && end.checked && !reverse.checked){
         link_van_naar_mijn_locatie_kies_bestemming.style.display = "block";
-        $(copy-route).text = $(link_van_naar).attr('href');
+        $("#copyRoute").val($(link_van_naar_mijn_locatie_kies_bestemming).attr('href'));
     }
     else if (!start.checked  && !end.checked && reverse.checked){
         link_naar_van.style.display = "block";
-        $(copy-route).text = $(link_van_naar).attr('href');
+        $("#copyRoute").val($(link_naar_van).attr('href'));
     }
     else if (start.checked  && !end.checked && reverse.checked){
         link_naar_van_mijn_locatie.style.display = "block";
-        $(copy-route).text = $(link_van_naar).attr('href');
+        $("#copyRoute").val($(link_naar_van_mijn_locatie).attr('href'));
     }
     else if (!start.checked  && end.checked && reverse.checked){
         link_naar_van_kies_bestemming.style.display = "block";
-        $(copy-route).text = $(link_van_naar).attr('href');
+        $("#copyRoute").val($(link_naar_van_kies_bestemming).attr('href'));
     }
     else if (start.checked  && end.checked && reverse.checked){
         link_naar_van_mijn_locatie_kies_bestemming.style.display = "block";
-        $(copy-route).text = $(link_naar_van_mijn_locatie_kies_bestemming).attr('href');
+        $("#copyRoute").val($(link_naar_van_mijn_locatie_kies_bestemming).attr('href'));
     }
+
+    $("#copyRoute").hide();
+
 }
 
 function myCopyFunction() {
-    var copyText = document.getElementById("copy-to-clipboard");
+    var copyText = document.getElementById("copyRoute");
     copyText.style.display = "block";
     copyText.select();
     copyText.setSelectionRange(0, 99999)
@@ -84,8 +89,32 @@ function myCopyFunction() {
 }
 
 function myCopyFunctionAll() {
-    var copyText = document.getElementById("copy-to-clipboard");
+    var copyText = document.getElementById("copyRoute");
     copyText.style.display = "block";
+
+    var toCopy = "";
+
+    toCopy += "Route:\n";
+    toCopy += ($("#link_van_naar").attr('href'));
+    toCopy += "\n\nRoute vanaf uw locatie:\n";
+    toCopy += ($("#link_van_naar_mijn_locatie").attr('href'));
+    toCopy += "\n\nRoute met te kiezen bestemming:\n";
+    toCopy += ($("#link_van_naar_kies_bestemming").attr('href'));
+    toCopy += "\n\nRoute vanaf uw locatie met te kiezen bestemming:\n";
+    toCopy += ($("#link_van_naar_mijn_locatie_kies_bestemming").attr('href'));
+    toCopy += "\n\nOmgekeerde route:\n";
+    toCopy += ($("#link_naar_van").attr('href'));
+    toCopy += "\n\nOmgekeerde route vanaf uw locatie:\n";
+    toCopy += ($("#link_naar_van_mijn_locatie").attr('href'));
+    toCopy += "\n\nOmgekeerde route met te kiezen bestemming:\n";
+    toCopy += ($("#link_naar_van_kies_bestemming").attr('href'));
+    toCopy += "\n\nOmgekeerde route vanaf uw locatie met te kiezen bestemming:\n";
+    toCopy += ($("#link_naar_van_mijn_locatie_kies_bestemming").attr('href'));
+
+
+    $("#copyRoute").val(toCopy);
+
+
     copyText.select();
     copyText.setSelectionRange(0, 99999)
     document.execCommand("copy");
